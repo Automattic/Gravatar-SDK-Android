c0e6850772b09be8a065a515780dd07178de0ce2
Maxime Biais <maxime.biais@gmail.com>
2024-01-19 17:27:12 +0100

    Move constants to their own file

---
+++ gravatar/src/main/java/com/gravatar/GravatarConstants.kt
@@ -0,0 +1,11 @@
+package com.gravatar
+
+object GravatarConstants {
+    // Gravatar image / avatar
+    const val GRAVATAR_IMAGE_BASE_URL = "https://www.gravatar.com/"
+    const val GRAVATAR_IMAGE_HOST = "www.gravatar.com"
+    const val GRAVATAR_IMAGE_PATH = "avatar"
+
+    // Gravatar API
+    const val GRAVATAR_API_BASE_URL = "https://api.gravatar.com/v1/"
+}
