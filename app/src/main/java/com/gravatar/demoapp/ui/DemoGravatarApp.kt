51d5d946b1a9253890bfc17b570b8053ab5af10c
Héctor Abraham <hector.abraham.morillo.prieto@automattic.com>
2024-01-26 12:15:53 +0100

    Added DefaultGravatarImage picker in the configuration panel

--- app/src/main/java/com/gravatar/demoapp/ui/DemoGravatarApp.kt
+++ app/src/main/java/com/gravatar/demoapp/ui/DemoGravatarApp.kt
@@ -8,7 +8,12 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
+import androidx.compose.material3.Checkbox
 import androidx.compose.material3.Divider
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExposedDropdownMenuBox
+import androidx.compose.material3.ExposedDropdownMenuDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
@@ -25,6 +30,7 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
+import com.gravatar.DefaultAvatarImage
 import com.gravatar.R
 import com.gravatar.demoapp.theme.GravatarDemoAppTheme
 import com.gravatar.emailAddressToGravatarUrl
@@ -34,6 +40,9 @@ fun DemoGravatarApp() {
     var email by remember { mutableStateOf("gravatarMailHere@gravatar.com") }
     var gravatarUrl by remember { mutableStateOf("") }
     var avatarSize by remember { mutableStateOf<Int?>(null) }
+    var defaultAvatarImageEnabled by remember { mutableStateOf(false) }
+    var selectedDefaultAvatar by remember { mutableStateOf(DefaultAvatarImage.MONSTER) }
+    val defaultAvatarOptions = DefaultAvatarImage.entries
 
     GravatarDemoAppTheme {
         Column(
@@ -48,7 +57,20 @@ fun DemoGravatarApp() {
                 size = avatarSize,
                 onEmailChanged = { email = it },
                 onSizeChange = { avatarSize = it },
-                onLoadGravatarClicked = { gravatarUrl = emailAddressToGravatarUrl(email = email, size = avatarSize) },
+                onLoadGravatarClicked = {
+                    gravatarUrl = emailAddressToGravatarUrl(
+                        email = email,
+                        size = avatarSize,
+                        defaultAvatarImage = if (defaultAvatarImageEnabled) selectedDefaultAvatar else null,
+                    )
+                },
+                onDefaultAvatarImageEnabledChanged = {
+                    defaultAvatarImageEnabled = it
+                },
+                defaultAvatarImageEnabled = defaultAvatarImageEnabled,
+                selectedDefaultAvatarImage = selectedDefaultAvatar,
+                onDefaultAvatarImageChanged = { selectedDefaultAvatar = it },
+                defaultAvatarOptions = defaultAvatarOptions,
             )
 
             if (gravatarUrl.isNotEmpty()) {
@@ -65,9 +87,7 @@ fun DemoGravatarApp() {
 }
 
 @Composable
-fun GravatarDivider() {
-    Divider(thickness = 1.dp, color = Color.Black, modifier = Modifier.padding(vertical = 8.dp))
-}
+fun GravatarDivider() = Divider(thickness = 1.dp, color = Color.Black, modifier = Modifier.padding(vertical = 8.dp))
 
 @Composable
 fun GravatarGeneratedUrl(gravatarUrl: String) {
@@ -79,12 +99,7 @@ fun GravatarGeneratedUrl(gravatarUrl: String) {
 }
 
 @Composable
-fun GravatarImage(gravatarUrl: String) {
-    AsyncImage(
-        model = gravatarUrl,
-        contentDescription = null,
-    )
-}
+fun GravatarImage(gravatarUrl: String) = AsyncImage(model = gravatarUrl, contentDescription = null)
 
 @Composable
 fun GravatarImageSettings(
@@ -93,18 +108,75 @@ fun GravatarImageSettings(
     onEmailChanged: (String) -> Unit,
     onSizeChange: (Int?) -> Unit,
     onLoadGravatarClicked: () -> Unit,
+    defaultAvatarImageEnabled: Boolean,
+    onDefaultAvatarImageEnabledChanged: (Boolean) -> Unit,
+    selectedDefaultAvatarImage: DefaultAvatarImage,
+    onDefaultAvatarImageChanged: (DefaultAvatarImage) -> Unit,
+    defaultAvatarOptions: List<DefaultAvatarImage>,
 ) {
     GravatarEmailInput(email = email, onValueChange = onEmailChanged, Modifier.fillMaxWidth())
-    Row(Modifier.padding(vertical = 8.dp)) {
-        TextField(
-            value = size?.toString() ?: "",
-            onValueChange = { value -> onSizeChange(value.toIntOrNull()) },
-            label = { Text(stringResource(R.string.gravatar_size_input_label)) },
-            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+    DefaultAvatarImageDropdown(
+        enabled = defaultAvatarImageEnabled,
+        selectedOption = selectedDefaultAvatarImage,
+        onEnabledChanged = onDefaultAvatarImageEnabledChanged,
+        onSelectedOptionChange = onDefaultAvatarImageChanged,
+        defaultAvatarOptions = defaultAvatarOptions,
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(vertical = 8.dp),
+    )
+    TextField(
+        value = size?.toString() ?: "",
+        onValueChange = { value -> onSizeChange(value.toIntOrNull()) },
+        label = { Text(stringResource(R.string.gravatar_size_input_label)) },
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+        modifier = Modifier.padding(vertical = 8.dp),
+    )
+    Button(onClick = onLoadGravatarClicked) { Text(text = "Load Gravatar") }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun DefaultAvatarImageDropdown(
+    enabled: Boolean,
+    onEnabledChanged: (Boolean) -> Unit,
+    selectedOption: DefaultAvatarImage,
+    onSelectedOptionChange: (DefaultAvatarImage) -> Unit,
+    defaultAvatarOptions: List<DefaultAvatarImage>,
+    modifier: Modifier = Modifier,
+) {
+    var expanded by remember { mutableStateOf(false) }
+    Row(modifier = modifier) {
+        Checkbox(
+            checked = enabled,
+            onCheckedChange = {
+                if (enabled) {
+                    expanded = false
+                }
+                onEnabledChanged(!enabled)
+            },
         )
-    }
-    Button(onClick = onLoadGravatarClicked) {
-        Text(text = "Load Gravatar")
+        ExposedDropdownMenuBox(
+            expanded = expanded,
+            onExpandedChange = { expanded = !expanded },
+        ) {
+            TextField(
+                readOnly = true,
+                value = selectedOption.style,
+                onValueChange = { },
+                label = { Text(stringResource(R.string.default_avatar_image_label)) },
+                trailingIcon = { ExposedDropdownMenuDefaults.TrailingIcon(expanded = expanded) },
+                modifier = Modifier.menuAnchor(),
+            )
+            ExposedDropdownMenu(expanded = expanded, onDismissRequest = { expanded = false }) {
+                defaultAvatarOptions.forEach { selectionOption ->
+                    DropdownMenuItem(text = { Text(text = selectionOption.style) }, onClick = {
+                        onSelectedOptionChange.invoke(selectionOption)
+                        expanded = false
+                    })
+                }
+            }
+        }
     }
 }
 
