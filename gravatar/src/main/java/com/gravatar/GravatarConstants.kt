21cebe17d8efbd25e1577c21c00935fe31dae85b
Maxime Biais <maxime.biais@gmail.com>
2024-02-13 09:33:43 +0100

    Remove unecessary constants

--- gravatar/src/main/java/com/gravatar/GravatarConstants.kt
+++ gravatar/src/main/java/com/gravatar/GravatarConstants.kt
@@ -1,9 +1,6 @@
 package com.gravatar
 
 object GravatarConstants {
-    /** Gravatar image base URL */
-    const val GRAVATAR_IMAGE_BASE_URL = "https://www.gravatar.com/"
-
     /** Gravatar image host */
     const val GRAVATAR_IMAGE_HOST = "www.gravatar.com"
 
@@ -15,13 +12,4 @@ object GravatarConstants {
 
     /** Gravatar API base URL */
     const val GRAVATAR_API_BASE_URL = "https://api.gravatar.com/v1/"
-
-    /** Minimum size of the avatar */
-    const val MINIMUM_AVATAR_SIZE = 1
-
-    /** Maximum size of the avatar */
-    const val MAXIMUM_AVATAR_SIZE = 2048
-
-    /** Range of the avatar size */
-    val AVATAR_SIZE_RANGE = MINIMUM_AVATAR_SIZE..MAXIMUM_AVATAR_SIZE
 }
