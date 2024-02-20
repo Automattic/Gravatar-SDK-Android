92c8b915a41f889b1887dc18ddb7b0dd855b6057
Maxime Biais <maxime.biais@gmail.com>
2024-02-20 15:24:31 +0100

    Add gson converter for retrofit

--- gravatar/build.gradle.kts
+++ gravatar/build.gradle.kts
@@ -68,6 +68,7 @@ android {
 dependencies {
     implementation("com.squareup.okhttp3:okhttp:4.12.0")
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
 
     testImplementation("junit:junit:4.13.2")
