e66496af40b04eeea419449d8eb1d9e7c653c163
Héctor Abraham <hector.abraham.morillo.prieto@automattic.com>
2024-02-28 17:18:00 +0100

    Create a composable to simplify the avatar update flow
    This commit implements a composable inside the library that simplifies the process of picking, cropping, and uploading a new image as an avatar.
    
    Another composable can be passed as a parameter. The GravatarImagePickerWrapper will wrap up that composable, allowing the developer to customize their UI.

--- gravatar/build.gradle.kts
+++ gravatar/build.gradle.kts
@@ -75,6 +75,7 @@ dependencies {
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
+    implementation("com.github.yalantis:ucrop:2.2.8")
 
     // Jetpack Compose
     implementation(platform("androidx.compose:compose-bom:2024.02.00"))
---
+++ gravatar/src/main/java/com/gravatar/ui/GravatarImagePickerWrapper.kt
@@ -0,0 +1,96 @@
+package com.gravatar.ui
+
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.PickVisualMediaRequest
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.clickable
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.neverEqualPolicy
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.core.net.toFile
+import com.gravatar.GravatarApi
+import com.yalantis.ucrop.UCrop
+import com.yalantis.ucrop.UCropActivity
+import java.io.File
+
+@Composable
+public fun GravatarImagePickerWrapper(
+    content: @Composable () -> Unit,
+    email: String,
+    accessToken: String,
+    listener: GravatarImagePickerWrapperListener,
+    modifier: Modifier = Modifier,
+    imageEditionOptions: ImageEditionOptions = ImageEditionOptions(),
+) {
+    var selectedImageUri by remember { mutableStateOf<Uri?>(null, neverEqualPolicy()) }
+    val imagePickerLauncher = rememberLauncherForActivityResult(ActivityResultContracts.PickVisualMedia()) {
+        selectedImageUri = it
+    }
+    val uCropLauncher = rememberLauncherForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+        it.data?.let { intentData ->
+            UCrop.getOutput(intentData)?.let { croppedImageUri ->
+                listener.onAvatarUploadStarted()
+                GravatarApi().uploadGravatar(croppedImageUri.toFile(), email, accessToken, listener)
+            }
+        }
+    }
+
+    Surface(
+        modifier = modifier.clickable {
+            imagePickerLauncher.launch(
+                PickVisualMediaRequest(mediaType = ActivityResultContracts.PickVisualMedia.ImageOnly),
+            )
+        },
+    ) {
+        content()
+    }
+
+    selectedImageUri?.let { image ->
+        selectedImageUri = null
+        launchAvatarCrop(image, uCropLauncher, LocalContext.current, imageEditionOptions)
+    }
+}
+
+private fun launchAvatarCrop(
+    image: Uri,
+    uCropLauncher: ActivityResultLauncher<Intent>,
+    context: Context,
+    imageEditionOptions: ImageEditionOptions,
+) {
+    val options = UCrop.Options().apply {
+        with(imageEditionOptions) {
+            toolbarColor?.let { setToolbarColor(it) }
+            statusBarColor?.let { setStatusBarColor(it) }
+            toolbarWidgetColor?.let { setToolbarWidgetColor(it) }
+        }
+        setShowCropGrid(false)
+        setAllowedGestures(UCropActivity.SCALE, UCropActivity.NONE, UCropActivity.NONE)
+        setHideBottomControls(true)
+    }
+    uCropLauncher.launch(
+        UCrop.of(image, Uri.fromFile(File(context.cacheDir, "cropped_for_gravatar.jpg")))
+            .withAspectRatio(1f, 1f)
+            .withOptions(options)
+            .getIntent(context),
+    )
+}
+
+public interface GravatarImagePickerWrapperListener : GravatarApi.GravatarListener<Unit> {
+    public fun onAvatarUploadStarted()
+}
+
+public data class ImageEditionOptions(
+    val statusBarColor: Int? = null,
+    val toolbarColor: Int? = null,
+    val toolbarWidgetColor: Int? = null,
+)
