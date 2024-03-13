5e5d2bdbd6bc7b591163f7bc5fe51f9ce0ee49a8
Héctor Abraham <hector.abraham.morillo.prieto@automattic.com>
2024-03-13 11:10:54 +0100

    Bottom sheet with the list of contacts

--- events/src/main/java/com/gravatar/events/MainActivity.kt
+++ events/src/main/java/com/gravatar/events/MainActivity.kt
@@ -3,53 +3,93 @@ package com.gravatar.events
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.BottomSheetScaffold
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.SheetValue
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.rememberBottomSheetScaffoldState
+import androidx.compose.material3.rememberStandardBottomSheetState
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.gravatar.events.ui.theme.GravatarTheme
 import com.gravatar.models.UserProfile
 import com.gravatar.ui.components.ProfileCard
+import com.gravatar.ui.components.ProfileListItem
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            GravatarTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    ProfileCard(
-                        UserProfile(
-                            "61b4e17387f9a143d28f5083988b2999",
-                            displayName = "John Doe",
-                            aboutMe = "I'm John Doe",
-                        ),
-                        modifier = Modifier.padding(16.dp),
-                        avatarImageSize = 128.dp,
+            EventsApp()
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun EventsApp() {
+    GravatarTheme {
+        // A surface container using the 'background' color from the theme
+        Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
+            val bottomSheetScaffoldState = rememberBottomSheetScaffoldState(
+                bottomSheetState = rememberStandardBottomSheetState(
+                    initialValue = SheetValue.PartiallyExpanded
+                )
+            )
+
+            BottomSheetScaffold(
+                sheetContent = {
+                    Column(modifier = Modifier.fillMaxWidth()) {
+                        ProfileCard(
+                            UserProfile(
+                                "61b4e17387f9a143d28f5083988b2999",
+                                displayName = "John Doe",
+                                aboutMe = "I'm John Doe",
+                            ),
+                            modifier = Modifier
+                                .align(Alignment.CenterHorizontally),
+                            avatarImageSize = 64.dp,
+                        )
+                    }
+                    ProfilesList(
+                        profiles = listOf(
+                            UserProfile(hash = "a", displayName = "John Doe", aboutMe = "Software Engineer"),
+                            UserProfile(hash = "b", displayName = "Daniel Smith", aboutMe = "Artist and Youtuber"),
+                            UserProfile(hash = "c", displayName = "Mary Johnson", aboutMe = "Artist and Youtuber"),
+                            UserProfile(hash = "d", displayName = "Robert Brown", aboutMe = "Artist and Youtuber"),
+                            UserProfile(hash = "e", displayName = "Clarence White", aboutMe = "Artist and Youtuber"),
+                            UserProfile(hash = "f", displayName = "Ana", aboutMe = "Artist and Youtuber"),
+                            UserProfile(hash = "g", displayName = "Erika", aboutMe = "Artist and Youtuber"),
+                            UserProfile(hash = "h", displayName = "Sophia", aboutMe = "Artist and Youtuber"),
+                            UserProfile(hash = "i", displayName = "Charles", aboutMe = "Artist and Youtuber"),
+                            UserProfile(hash = "g", displayName = "Eneko", aboutMe = "Artist and Youtuber"),
+                        )
                     )
-                }
-            }
+                },
+                sheetPeekHeight = 500.dp,
+                scaffoldState = bottomSheetScaffoldState,
+                content = {
+                    Text(text = "Content here")
+                },
+            )
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier,
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    GravatarTheme {
-        Greeting("Android")
+fun ProfilesList(profiles: List<UserProfile>) {
+    LazyColumn {
+        items(profiles.size) { index ->
+            ProfileListItem(modifier = Modifier.padding(8.dp), profile = profiles[index], avatarImageSize = 56.dp)
+        }
     }
 }
---
+++ gravatar/src/main/java/com/gravatar/ui/components/ProfileListItem.kt
@@ -0,0 +1,78 @@
+package com.gravatar.ui.components
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
+import com.gravatar.DefaultAvatarImage
+import com.gravatar.gravatarUrl
+import com.gravatar.models.Email
+import com.gravatar.models.UserProfile
+
+@Composable
+public fun ProfileListItem(profile: UserProfile, modifier: Modifier = Modifier, avatarImageSize: Dp = 128.dp) {
+    Row(
+        modifier = modifier,
+        verticalAlignment = Alignment.CenterVertically,
+    ) {
+        AsyncImage(
+            gravatarUrl(
+                profile.hash ?: "",
+                size = with(LocalDensity.current) { avatarImageSize.toPx().toInt() },
+                defaultAvatarImage = DefaultAvatarImage.Monster,
+            ),
+            contentDescription = "User profile image",
+            modifier = Modifier
+                .clip(CircleShape)
+                .size(avatarImageSize),
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Column(Modifier.padding(start = 8.dp)) {
+            (profile.displayName ?: profile.preferredUsername)?.let {
+                Text(
+                    text = it,
+                    style = MaterialTheme.typography.titleMedium,
+                    textAlign = TextAlign.Center,
+                )
+            }
+
+            profile.aboutMe?.let {
+                Text(
+                    text = it,
+                    style = MaterialTheme.typography.bodyMedium,
+                    textAlign = TextAlign.Center,
+                )
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun ProfileListItemPreview() {
+    ProfileListItem(
+        UserProfile(
+            displayName = "John Doe",
+            preferredUsername = "johndoe",
+            aboutMe = "I'm a farmer who loves to code",
+            emails = arrayListOf(Email(primary = true, value = "john@doe.com")),
+        ),
+    )
+}
