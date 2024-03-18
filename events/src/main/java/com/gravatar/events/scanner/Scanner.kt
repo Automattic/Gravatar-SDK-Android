3bf6c931d96282f0580fd108ebdc46b9e9222e66
Maxime Lumeau <maxime.lumeau@automattic.com>
2024-03-18 09:31:14 +0100

    Refactor scanner to init in a LaunchedEffect

--- events/src/main/java/com/gravatar/events/scanner/Scanner.kt
+++ events/src/main/java/com/gravatar/events/scanner/Scanner.kt
@@ -14,27 +14,26 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableLongStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.core.content.ContextCompat
 import com.gravatar.events.gravatar.parseGravatarHash
-import kotlinx.coroutines.launch
 import java.util.concurrent.Executor
-import java.util.concurrent.ExecutorService
-import java.util.concurrent.Executors
 import kotlin.coroutines.resume
 import kotlin.coroutines.suspendCoroutine
 
@@ -71,67 +70,60 @@ fun ScannerCamera(
     enablePreview: Boolean = true, // Default enable camera preview
     codeFoundListener: (String) -> Unit
 ) {
-    val coroutineScope = rememberCoroutineScope()
     val lifecycleOwner = LocalLifecycleOwner.current
-
+    val context = LocalContext.current
     var lastScanTime by remember { mutableLongStateOf(0L) }
-
     val cameraPreviewEnabled by remember { mutableStateOf(enablePreview) }
+    var previewView: PreviewView? = null
 
     AndroidView(
         modifier = modifier,
-        factory = { context ->
-            val previewView = PreviewView(context).apply {
+        factory = {
+            previewView = PreviewView(context).apply {
                 this.scaleType = scaleType
                 layoutParams = ViewGroup.LayoutParams(
                     ViewGroup.LayoutParams.MATCH_PARENT,
                     ViewGroup.LayoutParams.MATCH_PARENT
                 )
             }
+            previewView!!
+    })
 
-            // CameraX Preview UseCase
-            val previewUseCase = Preview.Builder()
-                .build()
-                .also {
-                    it.setSurfaceProvider(previewView.surfaceProvider)
-                }
+    LaunchedEffect(enablePreview) {
+        val cameraProvider = context.getCameraProvider()
 
-            coroutineScope.launch {
-                val cameraProvider = context.getCameraProvider()
+        // CameraX Preview UseCase
+        val previewUseCase = Preview.Builder()
+            .build()
+            .also { it.setSurfaceProvider(previewView?.surfaceProvider) }
 
-                val imageAnalysis = ImageAnalysis.Builder()
-                    .setBackpressureStrategy(ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST)
-                    .build()
+        val imageAnalysis = ImageAnalysis.Builder()
+            .setBackpressureStrategy(ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST)
+            .build()
 
-                val cameraExecutor: ExecutorService = Executors.newSingleThreadExecutor()
-
-                imageAnalysis.setAnalyzer(cameraExecutor, QRCodeAnalyzer { qrCodes ->
-                    qrCodes.firstOrNull()?.let { code ->
-                        // Debounce scanning
-                        val currentTime = System.currentTimeMillis()
-                        if (currentTime - lastScanTime >= debounceTimeMillis) {
-                            lastScanTime = System.currentTimeMillis()
-                            code.rawValue?.let { codeFoundListener(it) }
-                        }
-                    }
-                })
-
-                try {
-                    // Must unbind the use-cases before rebinding them.
-                    cameraProvider.unbindAll()
-                    if (cameraPreviewEnabled) {
-                        cameraProvider.bindToLifecycle(
-                            lifecycleOwner, cameraSelector, previewUseCase, imageAnalysis
-                        )
-                    }
-                } catch (ex: Exception) {
-                    Log.e("CameraPreview", "Use case binding failed", ex)
+        imageAnalysis.setAnalyzer(context.executor, QRCodeAnalyzer { qrCodes ->
+            qrCodes.firstOrNull()?.let { code ->
+                // Debounce scanning
+                val currentTime = System.currentTimeMillis()
+                if (currentTime - lastScanTime >= debounceTimeMillis) {
+                    lastScanTime = System.currentTimeMillis()
+                    code.rawValue?.let { codeFoundListener(it) }
                 }
             }
+        })
 
-            previewView
+        try {
+            // Must unbind the use-cases before rebinding them.
+            cameraProvider.unbindAll()
+            if (cameraPreviewEnabled) {
+                cameraProvider.bindToLifecycle(
+                    lifecycleOwner, cameraSelector, previewUseCase, imageAnalysis
+                )
+            }
+        } catch (ex: Exception) {
+            Log.e("CameraPreview", "Use case binding failed", ex)
         }
-    )
+    }
 }
 
 @Composable
@@ -164,3 +156,8 @@ suspend fun Context.getCameraProvider(): ProcessCameraProvider = suspendCoroutin
 val Context.executor: Executor
     get() = ContextCompat.getMainExecutor(this)
 
+@Composable
+@androidx.compose.ui.tooling.preview.Preview
+fun ScannerPreview() {
+    Scanner(modifier = Modifier.height(320.dp), onCodeScanned = {})
+}
