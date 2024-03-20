dfa4fa2c1bfaae377fd81db1a2ef678b7217663d
Maxime Biais <maxime.biais@gmail.com>
2024-03-20 11:55:44 +0100

    Add ProfileUrl class

--- gravatar/src/main/java/com/gravatar/ProfileUrl.kt
+++ gravatar/src/main/java/com/gravatar/ProfileUrl.kt
@@ -1,3 +1,19 @@
 package com.gravatar
 
-class ProfileUrl
+import android.net.Uri
+import com.gravatar.types.Email
+import com.gravatar.types.Hash
+
+public class ProfileUrl(public val hash: Hash) {
+    public val url: Uri = Uri.Builder()
+        .scheme("https")
+        .authority(GravatarConstants.GRAVATAR_BASE_HOST)
+        .appendPath(hash.toString())
+        .build()
+
+    public constructor(email: Email) : this(email.hash())
+
+    public fun avatarUrl(): AvatarUrl {
+        return AvatarUrl(hash)
+    }
+}
