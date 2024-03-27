73384858a158f2e67509c82a123c3dc7694853b2
Maxime Biais <maxime.biais@gmail.com>
2024-03-27 14:10:18 +0100

    New AboutMe and DisplayName atomic components

--- gravatar-ui/src/main/java/com/gravatar/ui/components/atomic/Avatar.kt
+++ gravatar-ui/src/main/java/com/gravatar/ui/components/atomic/Avatar.kt
@@ -4,7 +4,9 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
 import com.gravatar.AvatarQueryOptions
 import com.gravatar.api.models.UserProfile
@@ -28,4 +30,10 @@ public fun Avatar(
         contentDescription = "User profile image",
         modifier = modifier.size(size),
     )
+}
+
+@Preview
+@Composable
+private fun PreviewAvatar() {
+    Avatar(UserProfile("4539566a0223b11d28fc47c864336fa27b8fe49b5f85180178c9e3813e910d6a"), 256.dp)
 }
---
+++ gravatar-ui/src/main/java/com/gravatar/ui/components/atomic/DisplayName.kt
@@ -0,0 +1,11 @@
+package com.gravatar.ui.components.atomic
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.gravatar.api.models.UserProfile
+
+@Composable
+public fun DisplayName(profile: UserProfile, modifier: Modifier = Modifier) {
+    Text(text = profile.displayName ?: "", modifier = modifier)
+}
