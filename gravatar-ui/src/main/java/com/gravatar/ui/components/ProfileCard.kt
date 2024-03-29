87c208390ed12ee9dea6dd9d61d412f46f291136
Maxime Biais <maxime.biais@gmail.com>
2024-03-29 10:26:38 +0100

    Remove unecessary text alignement

--- gravatar-ui/src/main/java/com/gravatar/ui/components/ProfileCard.kt
+++ gravatar-ui/src/main/java/com/gravatar/ui/components/ProfileCard.kt
@@ -16,7 +16,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -51,7 +50,6 @@ fun ProfileCard(profile: UserProfile, modifier: Modifier = Modifier) {
                     TextStyle(
                         fontSize = 14.sp,
                         lineHeight = 20.sp,
-                        textAlign = TextAlign.Left,
                         color = MaterialTheme.colorScheme.outline,
                     ),
                 ) {
@@ -60,7 +58,7 @@ fun ProfileCard(profile: UserProfile, modifier: Modifier = Modifier) {
             }
         }
         Spacer(modifier = Modifier.height(16.dp))
-        ProvideTextStyle(MaterialTheme.typography.bodyMedium.merge(textAlign = TextAlign.Left)) {
+        ProvideTextStyle(MaterialTheme.typography.bodyMedium) {
             AboutMe(profile)
         }
         Row(
