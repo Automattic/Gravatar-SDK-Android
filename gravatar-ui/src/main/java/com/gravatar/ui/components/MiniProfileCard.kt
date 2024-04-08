ef36d1370ffed46828e611176de890930d7497d1
Maxime Biais <maxime.biais@gmail.com>
2024-04-08 14:59:08 +0200

    Remove unecessary layout node and update padding notation
    Co-authored-by: Héctor Abraham Morillo Prieto <hector.abraham.morillo.prieto@automattic.com>

--- gravatar-ui/src/main/java/com/gravatar/ui/components/MiniProfileCard.kt
+++ gravatar-ui/src/main/java/com/gravatar/ui/components/MiniProfileCard.kt
@@ -1,6 +1,5 @@
 package com.gravatar.ui.components
 
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.height
@@ -9,7 +8,6 @@ import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.ProvideTextStyle
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.text.TextStyle
@@ -25,38 +23,31 @@ import com.gravatar.ui.components.atomic.ViewProfileButton
 
 @Composable
 fun MiniProfileCard(profile: UserProfile, modifier: Modifier = Modifier) {
-    Column(
-        modifier = modifier,
-        horizontalAlignment = Alignment.Start,
-        verticalArrangement = Arrangement.Top,
-    ) {
-        Row {
-            Avatar(
-                profile = profile,
-                size = 72.dp,
-                modifier = Modifier.clip(CircleShape),
-            )
-            Column(modifier = Modifier.padding(14.dp, 0.dp, 0.dp, 0.dp)) {
-                ProvideTextStyle(TextStyle(fontSize = 18.sp, fontWeight = FontWeight.Bold, lineHeight = 24.sp)) {
-                    DisplayName(profile)
-                }
-                ProvideTextStyle(
-                    TextStyle(
-                        fontSize = 14.sp,
-                        lineHeight = 10.sp,
-                        color = MaterialTheme.colorScheme.outline,
-                    ),
-                ) {
-                    Location(profile)
-                }
-                ProvideTextStyle(
-                    MaterialTheme.typography.bodyMedium.merge(color = MaterialTheme.colorScheme.onBackground),
-                ) {
-                    ViewProfileButton(
-                        profile,
-                        modifier = Modifier.height(32.dp),
-                    )
-                }
+    Row(modifier = modifier) {
+        Avatar(
+            profile = profile,
+            size = 72.dp,
+            modifier = Modifier.clip(CircleShape),
+        )
+        Column(modifier = Modifier.padding(start = 14.dp)) {
+            ProvideTextStyle(TextStyle(fontSize = 18.sp, fontWeight = FontWeight.Bold, lineHeight = 24.sp)) {
+                DisplayName(profile)
+            }
+            ProvideTextStyle(
+                TextStyle(
+                    fontSize = 14.sp,
+                    color = MaterialTheme.colorScheme.outline,
+                ),
+            ) {
+                Location(profile)
+            }
+            ProvideTextStyle(
+                MaterialTheme.typography.bodyMedium.merge(color = MaterialTheme.colorScheme.onBackground),
+            ) {
+                ViewProfileButton(
+                    profile,
+                    modifier = Modifier.height(32.dp),
+                )
             }
         }
     }
