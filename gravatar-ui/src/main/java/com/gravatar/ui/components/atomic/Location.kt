d42be5a48e25442b15d04913b85512acee894697
Héctor Abraham <hector.abraham.morillo.prieto@automattic.com>
2024-04-10 10:19:30 +0200

    Adding documentation for undocumented public composables

--- gravatar-ui/src/main/java/com/gravatar/ui/components/atomic/Location.kt
+++ gravatar-ui/src/main/java/com/gravatar/ui/components/atomic/Location.kt
@@ -5,6 +5,15 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.gravatar.api.models.UserProfile
 
+/**
+ * [Location] is a composable that displays a user's location in text format.
+ * The user's location is displayed in a text format. If the location is too long, it will be truncated
+ *
+ * @param profile The user's profile information
+ * @param modifier Composable modifier
+ * @param maxLines Maximum number of lines to display
+ * @param dialogContent Content to display in a dialog when the text is clicked
+ */
 @Composable
 public fun Location(
     profile: UserProfile,
