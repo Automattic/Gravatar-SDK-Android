ccccbe6f01ced6825ad19949534ee7c5c961dd2c
Maxime Biais <maxime.biais@gmail.com>
2024-04-19 08:19:20 +0200

    Set minSdk to 21

--- demo-app/build.gradle.kts
+++ demo-app/build.gradle.kts
@@ -28,7 +28,7 @@ android {
 
     defaultConfig {
         applicationId = "com.gravatar.demoapp"
-        minSdk = 24
+        minSdk = 21
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
