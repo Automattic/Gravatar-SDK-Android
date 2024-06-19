f4868561b00175a15bb762fb512ce6ac9c2b279e
Héctor Abraham <hector.abraham.morillo.prieto@automattic.com>
2024-06-19 11:57:25 +0200

    Screenshot tests with custom avatar and view profile buttons

--- gravatar-ui/src/test/java/com/gravatar/gravatar/ui/components/ProfileTest.kt
+++ gravatar-ui/src/test/java/com/gravatar/gravatar/ui/components/ProfileTest.kt
@@ -1,9 +1,14 @@
 package com.gravatar.gravatar.ui.components
 
+import androidx.compose.foundation.Image
+import androidx.compose.material3.Text
+import androidx.compose.ui.res.painterResource
 import com.gravatar.gravatar.ui.RoborazziTest
 import com.gravatar.gravatar.ui.completeProfile
+import com.gravatar.ui.R
 import com.gravatar.ui.components.ComponentState
 import com.gravatar.ui.components.Profile
+import com.gravatar.ui.gravatarTheme
 import org.junit.Test
 import org.robolectric.annotation.Config
 
@@ -21,4 +26,15 @@ class ProfileTest : RoborazziTest() {
 
     @Test
     fun profileLight() = gravatarScreenshotTest { Profile(ComponentState.Loaded(completeProfile)) }
+
+    @Test
+    fun profileWithCustomComponents() = gravatarScreenshotTest {
+        Profile(
+            state = ComponentState.Loaded(completeProfile),
+            avatar = { _ ->
+                Image(painter = painterResource(id = R.drawable.gravatar_icon), contentDescription = "")
+            },
+            viewProfile = { _ -> Text(text = "Custom Profile Button", color = gravatarTheme.colorScheme.primary) },
+        )
+    }
 }
