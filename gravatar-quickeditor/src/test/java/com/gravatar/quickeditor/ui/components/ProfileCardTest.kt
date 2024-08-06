d12322192056567d9ef1f5252bab399ae7b1c7d9
AdamGrzybkowski <agrzybkowski@outlook.com>
2024-08-06 12:02:16 +0200

    Create ProfileCard component

---
+++ gravatar-quickeditor/src/test/java/com/gravatar/quickeditor/ui/components/ProfileCardTest.kt
@@ -0,0 +1,43 @@
+package com.gravatar.quickeditor.ui.components
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.gravatar.extensions.defaultProfile
+import com.gravatar.quickeditor.ui.gravatarScreenshotTest
+import com.gravatar.ui.components.ComponentState
+import com.gravatar.uitestutils.RoborazziTest
+import org.junit.Test
+import org.robolectric.annotation.Config
+
+class ProfileCardTest : RoborazziTest() {
+    private val profile = defaultProfile(
+        hash = "hash",
+        displayName = "Henry Wong",
+        location = "London, UK",
+    )
+
+    @Test
+    fun profileCardLightMode() {
+        gravatarScreenshotTest {
+            Surface {
+                ProfileCard(
+                    profile = ComponentState.Loaded(profile),
+                    modifier = Modifier.padding(20.dp),
+                )
+            }
+        }
+    }
+
+    @Test
+    @Config(qualifiers = "+night")
+    fun profileCardDarkMode() = gravatarScreenshotTest {
+        Surface {
+            ProfileCard(
+                profile = ComponentState.Loaded(profile),
+                modifier = Modifier.padding(20.dp),
+            )
+        }
+    }
+}
