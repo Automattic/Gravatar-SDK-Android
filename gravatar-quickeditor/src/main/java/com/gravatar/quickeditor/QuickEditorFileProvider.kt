1f7babd6642c1b39a8ee605f0c398ce38329cba6
AdamGrzybkowski <agrzybkowski@outlook.com>
2024-08-14 14:26:59 +0200

    Setup QuickEditor file provider for taking pictures

---
+++ gravatar-quickeditor/src/main/java/com/gravatar/quickeditor/QuickEditorFileProvider.kt
@@ -0,0 +1,24 @@
+package com.gravatar.quickeditor
+
+import android.content.Context
+import android.net.Uri
+import android.util.Log
+import androidx.core.content.FileProvider
+import java.io.File
+
+internal class QuickEditorFileProvider : FileProvider(R.xml.quickeditor_filepaths) {
+    companion object {
+        fun getTempCameraImageUri(context: Context): Uri {
+            val directory = File(context.cacheDir, "quickeditor")
+            directory.mkdirs()
+            val file = File(directory, "temp_camera_image.jpg")
+            val authority = "${context.packageName}.com.quickeditor.fileprovider"
+            Log.d("QuickEditor", "Authority: $authority")
+            return getUriForFile(
+                context,
+                authority,
+                file,
+            )
+        }
+    }
+}
