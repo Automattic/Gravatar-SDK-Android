a61c9b2c280e28aa8a718fc52ddb4c22119a2cc9
AdamGrzybkowski <agrzybkowski@outlook.com>
2024-08-21 13:11:06 +0200

    Modify the error handling in the AvatarRespository

--- gravatar-quickeditor/src/main/java/com/gravatar/quickeditor/data/repository/AvatarRepository.kt
+++ gravatar-quickeditor/src/main/java/com/gravatar/quickeditor/data/repository/AvatarRepository.kt
@@ -7,6 +7,7 @@ import com.gravatar.quickeditor.data.storage.TokenStorage
 import com.gravatar.restapi.models.Avatar
 import com.gravatar.restapi.models.Identity
 import com.gravatar.services.AvatarService
+import com.gravatar.services.ErrorType
 import com.gravatar.services.IdentityService
 import com.gravatar.services.Result
 import com.gravatar.types.Email
@@ -26,12 +27,18 @@ internal class AvatarRepository(
     suspend fun getAvatars(email: Email): Result<IdentityAvatars, QuickEditorError> = withContext(dispatcher) {
         val token = tokenStorage.getToken(email.hash().toString())
         token?.let {
-            try {
-                val avatars = getAvatarsAsync(token).await()
-                val identity = getIdentityAsync(email, token).await()
-                Result.Success(IdentityAvatars(avatars, identity.imageId))
-            } catch (e: Exception) {
-                Result.Failure(QuickEditorError.Unknown)
+            val avatarsResult = getAvatarsAsync(token).await()
+            val identityResult = getIdentityAsync(email, token).await()
+            if (avatarsResult is Result.Success && identityResult is Result.Success) {
+                Result.Success(
+                    IdentityAvatars(
+                        avatarsResult.value,
+                        identityResult.value.imageId,
+                    ),
+                )
+            } else {
+                val quickEditorError: QuickEditorError = (avatarsResult to identityResult).quickEditorError
+                Result.Failure(quickEditorError)
             }
         } ?: Result.Failure(QuickEditorError.TokenNotFound)
     }
@@ -39,9 +46,9 @@ internal class AvatarRepository(
     suspend fun selectAvatar(email: Email, avatarId: String): Result<Unit, QuickEditorError> = withContext(dispatcher) {
         val token = tokenStorage.getToken(email.hash().toString())
         token?.let {
-            when (identityService.setAvatarCatching(email.hash().toString(), avatarId, token)) {
+            when (val result = identityService.setAvatarCatching(email.hash().toString(), avatarId, token)) {
                 is Result.Success -> Result.Success(Unit)
-                is Result.Failure -> Result.Failure(QuickEditorError.Server)
+                is Result.Failure -> Result.Failure(QuickEditorError.Request(result.error))
             }
         } ?: Result.Failure(QuickEditorError.TokenNotFound)
     }
@@ -49,21 +56,30 @@ internal class AvatarRepository(
     suspend fun uploadAvatar(email: Email, avatarUri: Uri): Result<Unit, QuickEditorError> = withContext(dispatcher) {
         val token = tokenStorage.getToken(email.hash().toString())
         token?.let {
-            when (avatarService.uploadCatching(avatarUri.toFile(), token)) {
+            when (val result = avatarService.uploadCatching(avatarUri.toFile(), token)) {
                 is Result.Success -> Result.Success(Unit)
-                is Result.Failure -> Result.Failure(QuickEditorError.AvatarUploadFailed)
+                is Result.Failure -> Result.Failure(QuickEditorError.Request(result.error))
             }
         } ?: Result.Failure(QuickEditorError.TokenNotFound)
     }
 
-    private suspend fun getAvatarsAsync(token: String): Deferred<List<Avatar>> = coroutineScope {
-        async { avatarService.retrieve(token) }
+    private suspend fun getAvatarsAsync(token: String): Deferred<Result<List<Avatar>, ErrorType>> = coroutineScope {
+        async { avatarService.retrieveCatching(token) }
     }
 
-    private suspend fun getIdentityAsync(email: Email, token: String): Deferred<Identity> = coroutineScope {
-        async { identityService.retrieve(hash = email.hash().toString(), oauthToken = token) }
-    }
+    private suspend fun getIdentityAsync(email: Email, token: String): Deferred<Result<Identity, ErrorType>> =
+        coroutineScope {
+            async { identityService.retrieveCatching(hash = email.hash().toString(), oauthToken = token) }
+        }
 }
+
+/**
+ * Take first or second if available, otherwise [QuickEditorError.Unknown]
+ */
+private val Pair<Result<List<Avatar>, ErrorType>, Result<Identity, ErrorType>>.quickEditorError: QuickEditorError
+    get() = ((this.first as? Result.Failure)?.error ?: (this.second as? Result.Failure)?.error)
+        ?.let { QuickEditorError.Request(it) }
+        ?: QuickEditorError.Unknown
 
 internal data class IdentityAvatars(
     val avatars: List<Avatar>,
