5c4cd6eb3afddba0f0e185448b336a1e7def3dcb
AdamGrzybkowski <agrzybkowski@outlook.com>
2024-08-27 14:42:39 +0200

    Bring back withMaxResultSize in UCrop settings that was lost in merge conflicts

--- gravatar-quickeditor/src/main/java/com/gravatar/quickeditor/ui/copperlauncher/UCropCropperLauncher.kt
+++ gravatar-quickeditor/src/main/java/com/gravatar/quickeditor/ui/copperlauncher/UCropCropperLauncher.kt
@@ -9,7 +9,8 @@ import com.yalantis.ucrop.UCrop
 import com.yalantis.ucrop.UCropActivity
 import java.io.File
 
-private const val UCROP_COMPRESSION_QUALITY = 100
+private const val UCROP_COMPRESSION_QUALITY = 75
+private const val UCROP_MAX_IMAGE_SIZE = 1080
 
 internal class UCropCropperLauncher : CropperLauncher {
     override fun launch(launcher: ActivityResultLauncher<Intent>, image: Uri, tempFile: File, context: Context) {
@@ -19,6 +20,7 @@ internal class UCropCropperLauncher : CropperLauncher {
             setToolbarWidgetColor(Color.WHITE)
             setAllowedGestures(UCropActivity.SCALE, UCropActivity.ROTATE, UCropActivity.NONE)
             setCompressionQuality(UCROP_COMPRESSION_QUALITY)
+            withMaxResultSize(UCROP_MAX_IMAGE_SIZE, UCROP_MAX_IMAGE_SIZE)
             setCircleDimmedLayer(true)
         }
         launcher.launch(
