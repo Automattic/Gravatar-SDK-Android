4210a5b77db90d790295758c145777211472fd61
AdamGrzybkowski <agrzybkowski@outlook.com>
2024-09-02 13:54:31 +0200

    Make MediaPickerPopup foucsable
    This makes the taps outside the popup area only dismiss the popup instead of also being propagated to the views below

--- gravatar-quickeditor/src/main/java/com/gravatar/quickeditor/ui/components/MediaPickerPopup.kt
+++ gravatar-quickeditor/src/main/java/com/gravatar/quickeditor/ui/components/MediaPickerPopup.kt
@@ -12,12 +12,12 @@ import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Popup
+import androidx.compose.ui.window.PopupProperties
 import com.gravatar.quickeditor.R
 import com.gravatar.ui.GravatarTheme
 
@@ -34,6 +34,7 @@ internal fun MediaPickerPopup(
         alignment = alignment,
         onDismissRequest = onDismissRequest,
         offset = offset,
+        properties = PopupProperties(focusable = true),
     ) {
         Surface(
             modifier = Modifier
