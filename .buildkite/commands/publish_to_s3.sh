7b1e6a1cb6719025ce3f3450487b0bd70b006b92
Wojtek Zieba <wojtek.zieba@automattic.com>
2024-09-09 15:57:15 +0200

    Improve code comment and code quality suggestions

--- .buildkite/commands/publish_to_s3.sh
+++ .buildkite/commands/publish_to_s3.sh
@@ -1,10 +1,12 @@
 #!/bin/bash -eu
 
-# The A8C_LIBS_* credentials are for the `a8c-libs` S3 bucket in a separate `a8c` account.
-# We use distinct names to prevent conflicts with the AWS credentials of our `a8c-apps` account (used for all our other AppsInfra buckets).
+# The A8C_LIBS_* credentials are for the `a8c-libs` S3 bucket in which we want to publish the SDK artifact,
+# and which lives in a separate `a8c` AWS account.
+# We don't use the standard AWS env names here to prevent conflicts with the AWS credentials
+# of our main `a8c-apps` account (used for all our other AppsInfra S3 buckets).
 
-export AWS_ACCESS_KEY=${A8C_LIBS_AWS_ACCESS_KEY}
-export AWS_SECRET_KEY=${A8C_LIBS_AWS_SECRET_KEY}
+export AWS_ACCESS_KEY="${A8C_LIBS_AWS_ACCESS_KEY}"
+export AWS_SECRET_KEY="${A8C_LIBS_AWS_SECRET_KEY}"
 
 echo "--- 🚀 Publishing"
 ./gradlew \
