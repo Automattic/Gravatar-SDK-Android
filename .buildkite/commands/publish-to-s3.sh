57dd76a0e83112a3ac867d41e6ba7e9e4a83c2cd
Wojtek Zieba <wojtek.zieba@automattic.com>
2024-09-09 16:11:28 +0200

    Remove parenthesis from `prepare_to_publish_to_s3_params`
    Possible fix for "Either tagName or both branch name and sha1 command line arguments needs to be provided!"

--- .buildkite/commands/publish-to-s3.sh
+++ .buildkite/commands/publish-to-s3.sh
@@ -10,10 +10,10 @@ export AWS_SECRET_KEY="${A8C_LIBS_AWS_SECRET_KEY}"
 
 echo "--- 🚀 Publishing"
 ./gradlew \
-:gravatar:prepareToPublishToS3 "$(prepare_to_publish_to_s3_params)" \
+:gravatar:prepareToPublishToS3 $(prepare_to_publish_to_s3_params) \
 :gravatar:publish \
-:gravatar-ui:prepareToPublishToS3 "$(prepare_to_publish_to_s3_params)" \
+:gravatar-ui:prepareToPublishToS3 $(prepare_to_publish_to_s3_params) \
 :gravatar-ui:publish \
-:gravatar-quickeditor:prepareToPublishToS3 "$(prepare_to_publish_to_s3_params)" \
+:gravatar-quickeditor:prepareToPublishToS3 $(prepare_to_publish_to_s3_params) \
 :gravatar-quickeditor:publish \
 --no-configuration-cache
