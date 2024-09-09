ee6e0414344962ed865494389cee937f31db6dc4
Wojtek Zieba <wojtek.zieba@automattic.com>
2024-09-09 15:15:10 +0200

    Export AWS credentials in `publish_to_s3` command (see comment for the context)

--- .buildkite/commands/publish_to_s3.sh
+++ .buildkite/commands/publish_to_s3.sh
@@ -1,5 +1,11 @@
 #!/bin/bash -eu
 
+# The A8C_LIBS_* credentials are for the `a8c-libs` S3 bucket in a separate `a8c` account.
+# We use distinct names to prevent conflicts with the AWS credentials of our `a8c-apps` account (used for all our other AppsInfra buckets).
+
+export AWS_ACCESS_KEY=${A8C_LIBS_AWS_ACCESS_KEY}
+export AWS_SECRET_KEY=${A8C_LIBS_AWS_SECRET_KEY}
+
 echo "--- 🚀 Publishing"
 ./gradlew \
 :gravatar:prepareToPublishToS3 $(prepare_to_publish_to_s3_params) \
