4473d21566f8ae5384472b8a90b009c23a239b91
Wojtek Zieba <wojtek.zieba@automattic.com>
2024-09-17 11:18:34 +0200

    Use release variant for lint

--- .buildkite/commands/lint.sh
+++ .buildkite/commands/lint.sh
@@ -2,9 +2,9 @@
 
 echo "--- 🧹 Linting"
 # Run lint on app module first, to make sure that the lint-results-debug.sarif file is always generated
-./gradlew :gravatar-quickeditor:lintDebug
+./gradlew :gravatar-quickeditor:lintRelease
 lint_exit_code=$?
 
-upload_sarif_to_github 'gravatar-quickeditor/build/reports/lint-results-debug.sarif'
+upload_sarif_to_github 'gravatar-quickeditor/build/reports/lint-results-release.sarif'
 
 exit $lint_exit_code
