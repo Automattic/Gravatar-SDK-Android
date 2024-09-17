7a9d7926451ce8fdfea800ed5c4dbe3224c80fd0
AdamGrzybkowski <agrzybkowski@outlook.com>
2024-09-17 10:33:14 +0200

    Remove unused code related to the picker

--- gravatar/src/main/java/com/gravatar/services/AvatarService.kt
+++ gravatar/src/main/java/com/gravatar/services/AvatarService.kt
@@ -3,7 +3,6 @@ package com.gravatar.services
 import com.gravatar.di.container.GravatarSdkContainer.Companion.instance
 import com.gravatar.logger.Logger
 import com.gravatar.restapi.models.Avatar
-import com.gravatar.types.Email
 import kotlinx.coroutines.withContext
 import okhttp3.MultipartBody
 import okhttp3.OkHttpClient
@@ -17,39 +16,6 @@ import com.gravatar.di.container.GravatarSdkContainer.Companion.instance as Grav
 public class AvatarService(private val okHttpClient: OkHttpClient? = null) {
     private companion object {
         const val LOG_TAG = "AvatarService"
-    }
-
-    private val service = GravatarSdkDI.getGravatarV1Service(okHttpClient)
-
-    /**
-     * Uploads a Gravatar image for the given email address.
-     *
-     * @param file The image file to upload
-     * @param email The email address to associate the image with
-     * @param wordpressBearerToken The bearer token for the user's WordPress/Gravatar account
-     */
-    @Deprecated("Use upload(file: File, oauthToken: String) instead", level = DeprecationLevel.WARNING)
-    public suspend fun upload(file: File, email: Email, wordpressBearerToken: String): Result<Unit, ErrorType> {
-        val identity = MultipartBody.Part.createFormData("account", email.toString())
-        val filePart =
-            MultipartBody.Part.createFormData("filedata", file.name, file.asRequestBody())
-
-        return runCatchingService {
-            withContext(GravatarSdkDI.dispatcherIO) {
-                val response = service.uploadImage("Bearer $wordpressBearerToken", identity, filePart)
-
-                if (response.isSuccessful) {
-                    Result.Success(Unit)
-                } else {
-                    // Log the response body for debugging purposes if the response is not successful
-                    Logger.w(
-                        LOG_TAG,
-                        "Network call unsuccessful trying to upload Gravatar: $response.body",
-                    )
-                    Result.Failure(errorTypeFromHttpCode(response.code()))
-                }
-            }
-        }
     }
 
     /**
