1b9a5f17834176f73313cf327773de62b64e2d89
AdamGrzybkowski <agrzybkowski@outlook.com>
2024-09-19 09:35:13 +0200

    Remove java.time code

--- gravatar/src/main/java/com/gravatar/services/AvatarUploadTimeoutInterceptor.kt
+++ gravatar/src/main/java/com/gravatar/services/AvatarUploadTimeoutInterceptor.kt
@@ -2,23 +2,22 @@ package com.gravatar.services
 
 import okhttp3.Interceptor
 import okhttp3.Response
-import java.time.Duration
 import java.util.concurrent.TimeUnit
 
-private const val TIMEOUT_DURATION = 5L
+private const val TIMEOUT_DURATION_MILLIS = 5 * 60 * 1000 // 5 minutes
 
 /**
  * Increases the timeout for the avatar upload request
  */
 internal class AvatarUploadTimeoutInterceptor(
-    private val timeout: Duration = Duration.ofMinutes(TIMEOUT_DURATION),
+    private val timeoutMillis: Int = TIMEOUT_DURATION_MILLIS,
 ) : Interceptor {
     override fun intercept(chain: Interceptor.Chain): Response {
         val request = chain.request()
         val newChain = if (request.method == "POST" && request.url.encodedPath == "/v3/me/avatars") {
-            chain.withConnectTimeout(timeout.toMillis().toInt(), TimeUnit.MILLISECONDS)
-                .withReadTimeout(timeout.toMillis().toInt(), TimeUnit.MILLISECONDS)
-                .withWriteTimeout(timeout.toMillis().toInt(), TimeUnit.MILLISECONDS)
+            chain.withConnectTimeout(timeoutMillis, TimeUnit.MILLISECONDS)
+                .withReadTimeout(timeoutMillis, TimeUnit.MILLISECONDS)
+                .withWriteTimeout(timeoutMillis, TimeUnit.MILLISECONDS)
         } else {
             chain
         }
