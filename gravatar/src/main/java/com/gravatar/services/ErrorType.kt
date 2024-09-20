c6ce8ba202792aa0ead28c6d41eac2f4527e012f
AdamGrzybkowski <agrzybkowski@outlook.com>
2024-09-20 15:30:52 +0200

    Rewrite ErrorType to sealed class and add InvalidRequest type

--- gravatar/src/main/java/com/gravatar/services/ErrorType.kt
+++ gravatar/src/main/java/com/gravatar/services/ErrorType.kt
@@ -1,49 +1,72 @@
 package com.gravatar.services
 
+import com.google.gson.Gson
 import com.gravatar.HttpResponseCode
+import com.gravatar.restapi.models.Error
 import java.net.SocketTimeoutException
 import java.net.UnknownHostException
+import java.util.Objects
 
-internal fun errorTypeFromHttpCode(code: Int): ErrorType = when (code) {
-    HttpResponseCode.HTTP_CLIENT_TIMEOUT -> ErrorType.TIMEOUT
-    HttpResponseCode.HTTP_NOT_FOUND -> ErrorType.NOT_FOUND
-    HttpResponseCode.HTTP_TOO_MANY_REQUESTS -> ErrorType.RATE_LIMIT_EXCEEDED
-    HttpResponseCode.UNAUTHORIZED -> ErrorType.UNAUTHORIZED
-    in HttpResponseCode.SERVER_ERRORS -> ErrorType.SERVER
-    else -> ErrorType.UNKNOWN
+internal fun HttpException.errorTypeFromHttpCode(gson: Gson): ErrorType = when (code) {
+    HttpResponseCode.HTTP_CLIENT_TIMEOUT -> ErrorType.Timeout
+    HttpResponseCode.HTTP_NOT_FOUND -> ErrorType.NotFound
+    HttpResponseCode.HTTP_TOO_MANY_REQUESTS -> ErrorType.RateLimitExceeded
+    HttpResponseCode.UNAUTHORIZED -> ErrorType.Unauthorized
+    HttpResponseCode.INVALID_REQUEST -> {
+        val error: Error? = runCatching {
+            gson.fromJson(rawErrorBody, Error::class.java)
+        }.getOrNull()
+        ErrorType.InvalidRequest(error)
+    }
+
+    in HttpResponseCode.SERVER_ERRORS -> ErrorType.Server
+    else -> ErrorType.Unknown
 }
 
-internal fun Throwable.errorType(): ErrorType {
+internal fun Throwable.errorType(gson: Gson): ErrorType {
     return when (this) {
-        is SocketTimeoutException -> ErrorType.TIMEOUT
-        is UnknownHostException -> ErrorType.NETWORK
-        is HttpException -> errorTypeFromHttpCode(code)
-        else -> ErrorType.UNKNOWN
+        is SocketTimeoutException -> ErrorType.Timeout
+        is UnknownHostException -> ErrorType.Network
+        is HttpException -> this.errorTypeFromHttpCode(gson)
+        else -> ErrorType.Unknown
     }
 }
 
 /**
  * Error types for Gravatar image upload
  */
-public enum class ErrorType {
+public sealed class ErrorType {
     /** server returned an error */
-    SERVER,
+    public data object Server : ErrorType()
 
     /** network request timed out */
-    TIMEOUT,
+    public data object Timeout : ErrorType()
 
     /** network is not available */
-    NETWORK,
+    public data object Network : ErrorType()
 
     /** User or hash not found */
-    NOT_FOUND,
+    public data object NotFound : ErrorType()
 
     /** User or hash not found */
-    RATE_LIMIT_EXCEEDED,
+    public data object RateLimitExceeded : ErrorType()
 
     /** User not authorized to perform given action **/
-    UNAUTHORIZED,
+    public data object Unauthorized : ErrorType()
 
     /** An unknown error occurred */
-    UNKNOWN,
+    public data object Unknown : ErrorType()
+
+    /**
+     * An error occurred while processing the request.
+     *
+     * @property error The detailed error that occurred, if returned from the server.
+     */
+    public class InvalidRequest(public val error: Error?) : ErrorType() {
+        override fun toString(): String = "InvalidRequest(error=$error)"
+
+        override fun equals(other: Any?): Boolean = other is InvalidRequest && error == other.error
+
+        override fun hashCode(): Int = Objects.hash(error)
+    }
 }
--- gravatar/src/test/java/com/gravatar/services/ErrorTypeTest.kt
+++ gravatar/src/test/java/com/gravatar/services/ErrorTypeTest.kt
@@ -1,34 +1,60 @@
 package com.gravatar.services
 
+import com.google.gson.Gson
 import com.gravatar.HttpResponseCode.HTTP_CLIENT_TIMEOUT
 import com.gravatar.HttpResponseCode.HTTP_NOT_FOUND
 import com.gravatar.HttpResponseCode.HTTP_TOO_MANY_REQUESTS
+import com.gravatar.HttpResponseCode.INVALID_REQUEST
 import com.gravatar.HttpResponseCode.SERVER_ERRORS
+import com.gravatar.restapi.models.Error
 import io.mockk.every
 import io.mockk.mockk
+import junit.framework.TestCase.assertEquals
 import org.junit.Test
+import retrofit2.Response
 import java.net.SocketTimeoutException
 import java.net.UnknownHostException
 
 class ErrorTypeTest {
+    private val gson = Gson()
+
+    private val errorBody = """
+        {
+            "error": "Only square images are accepted",
+            "code": "uncropped_image"
+        }
+    """.trimIndent()
+
     private val httpCodeToErrorTypeRelation = mutableListOf(
-        HTTP_CLIENT_TIMEOUT to ErrorType.TIMEOUT,
-        HTTP_NOT_FOUND to ErrorType.NOT_FOUND,
-        HTTP_TOO_MANY_REQUESTS to ErrorType.RATE_LIMIT_EXCEEDED,
-        600 to ErrorType.UNKNOWN,
+        HTTP_CLIENT_TIMEOUT to ErrorType.Timeout,
+        HTTP_NOT_FOUND to ErrorType.NotFound,
+        HTTP_TOO_MANY_REQUESTS to ErrorType.RateLimitExceeded,
+        600 to ErrorType.Unknown,
+        INVALID_REQUEST to ErrorType.InvalidRequest(
+            error = Error {
+                code = "uncropped_image"
+                error = "Only square images are accepted"
+            },
+        ),
     ).apply {
         SERVER_ERRORS.forEach { code ->
-            add(code to ErrorType.SERVER)
+            add(code to ErrorType.Server)
         }
     }
 
     @Test
     fun `given an http code when converting to error type then correct type is returned`() {
         httpCodeToErrorTypeRelation.forEach { (code, expectedErrorType) ->
+            val response = mockk<Response<Unit>>(relaxed = true) {
+                every { code() } returns code
+                every { errorBody() } returns mockk {
+                    every { string() } returns errorBody
+                }
+            }
             // When
-            val errorType = errorTypeFromHttpCode(code)
+            val errorType = HttpException(response).errorTypeFromHttpCode(gson)
             // Then
-            assert(errorType == expectedErrorType)
+            assertEquals(expectedErrorType, errorType)
         }
     }
 
@@ -36,21 +62,23 @@ class ErrorTypeTest {
     fun `given an exception when converting to error type then correct type is returned`() {
         // Given
         val exceptionToErrorTypeRelation = mutableListOf(
-            SocketTimeoutException() to ErrorType.TIMEOUT,
-            UnknownHostException() to ErrorType.NETWORK,
-            Exception() to ErrorType.UNKNOWN,
+            SocketTimeoutException() to ErrorType.Timeout,
+            UnknownHostException() to ErrorType.Network,
+            Exception() to ErrorType.Unknown,
         ).apply {
             httpCodeToErrorTypeRelation.forEach { (code, errorType) ->
-                val exception = mockk<HttpException>(relaxed = true)
-                every { exception.code } returns code
+                val exception = mockk<HttpException>(relaxed = true) {
+                    every { this@mockk.code } returns code
+                    every { this@mockk.rawErrorBody } returns errorBody
+                }
                 add(exception to errorType)
             }
         }
         exceptionToErrorTypeRelation.forEach { (exception, expectedErrorType) ->
             // When
-            val errorType = exception.errorType()
+            val errorType = exception.errorType(gson)
             // Then
-            assert(errorType == expectedErrorType)
+            assertEquals(expectedErrorType, errorType)
         }
     }
 }
