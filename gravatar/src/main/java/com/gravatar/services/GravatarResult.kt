b1ec5107a1396d66fd4012a9c6244c3d5d4edb72
AdamGrzybkowski <agrzybkowski@outlook.com>
2024-10-02 11:39:01 +0200

    Rename Result to GravatarResult

--- gravatar/src/main/java/com/gravatar/services/GravatarResult.kt
+++ gravatar/src/main/java/com/gravatar/services/GravatarResult.kt
@@ -3,7 +3,7 @@ package com.gravatar.services
 /**
  * Class representing the result of a network operation.
  */
-public sealed class Result<T, E> {
+public sealed class GravatarResult<T, E> {
     /**
      * Represents a successful fetch operation.
      *
@@ -11,7 +11,7 @@ public sealed class Result<T, E> {
      * @param E The error type
      * @property value The fetched value
      */
-    public data class Success<T, E>(public val value: T) : Result<T, E>()
+    public data class Success<T, E>(public val value: T) : GravatarResult<T, E>()
 
     /**
      * Represents a failed fetch operation.
@@ -20,7 +20,7 @@ public sealed class Result<T, E> {
      * @param E The error type
      * @property error The error that occurred
      */
-    public data class Failure<T, E>(public val error: E) : Result<T, E>()
+    public data class Failure<T, E>(public val error: E) : GravatarResult<T, E>()
 
     /**
      * Shortcut function to ignore a failure.
