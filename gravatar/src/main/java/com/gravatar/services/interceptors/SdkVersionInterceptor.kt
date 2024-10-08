04c6aa3bd0e5d49016d1b7b8d7d9c8d0fd4adb7c
Héctor Abraham <hector.abraham.morillo.prieto@automattic.com>
2024-10-08 12:30:10 +0200

    Add X-Source in the SdkVersionInterceptor
    When the appName is available, we should send the X-Source header with that value.

--- gravatar/src/main/java/com/gravatar/services/interceptors/SdkVersionInterceptor.kt
+++ gravatar/src/main/java/com/gravatar/services/interceptors/SdkVersionInterceptor.kt
@@ -1,16 +1,18 @@
 package com.gravatar.services.interceptors
 
 import com.gravatar.BuildConfig
+import com.gravatar.di.container.GravatarSdkContainer
 import okhttp3.Interceptor
 import okhttp3.Response
 
 internal class SdkVersionInterceptor : Interceptor {
     override fun intercept(chain: Interceptor.Chain): Response {
         return chain.proceed(
-            chain.request().newBuilder()
-                .addHeader("X-Platform", "Android")
-                .addHeader("X-SDK-Version", BuildConfig.SDK_VERSION)
-                .build(),
+            chain.request().newBuilder().apply {
+                addHeader("X-Platform", "Android")
+                addHeader("X-SDK-Version", BuildConfig.SDK_VERSION)
+                GravatarSdkContainer.instance.appName?.let { addHeader("X-Source", it) }
+            }.build(),
         )
     }
 }
