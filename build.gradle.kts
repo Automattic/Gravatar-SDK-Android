d1dc4b119121695c5ea74435432b061d2d129eac
AdamGrzybkowski <agrzybkowski@outlook.com>
2024-10-08 14:37:19 +0200

    Update the versionName to 2.0.0-rc1

--- build.gradle.kts
+++ build.gradle.kts
@@ -54,7 +54,7 @@ apiValidation {
 }
 
 // Semantic versioning for release version
-val versionName = "1.1.0"
+val versionName = "2.0.0-rc1"
 
 tasks.dokkaHtmlMultiModule {
     notCompatibleWithConfigurationCache("https://github.com/Kotlin/dokka/issues/2231")
