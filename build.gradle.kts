a071272bda1c2930787a923c0558649cdbdd5087
AdamGrzybkowski <agrzybkowski@outlook.com>
2024-11-29 13:16:44 +0100

    Only use clean tag for sdkVersion when no other commits are added on top

--- build.gradle.kts
+++ build.gradle.kts
@@ -47,7 +47,7 @@ apiValidation {
 }
 
 val sdkVersion = providers.exec {
-    commandLine("git", "describe", "--tags", "--abbrev=0")
+    commandLine("git", "describe", "--tags")
 }.standardOutput.asText.get().trim()
 
 extra["sdkVersion"] = sdkVersion
