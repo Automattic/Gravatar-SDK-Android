af553c0b2eec32fbafe75ec96915fe5b708b5cb7
AdamGrzybkowski <agrzybkowski@outlook.com>
2024-12-10 11:35:31 +0100

    Modify the PickerPopup to calculate it's position without passing the params

--- gravatar-quickeditor/src/main/java/com/gravatar/quickeditor/ui/components/PickerPopup.kt
+++ gravatar-quickeditor/src/main/java/com/gravatar/quickeditor/ui/components/PickerPopup.kt
@@ -7,7 +7,6 @@ import androidx.compose.animation.core.MutableTransitionState
 import androidx.compose.animation.core.Spring
 import androidx.compose.animation.core.spring
 import androidx.compose.animation.scaleIn
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
@@ -15,40 +14,35 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.Immutable
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.geometry.Rect
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.onGloballyPositioned
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.Density
 import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.DpOffset
 import androidx.compose.ui.unit.IntOffset
+import androidx.compose.ui.unit.IntRect
 import androidx.compose.ui.unit.IntSize
+import androidx.compose.ui.unit.LayoutDirection
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Popup
+import androidx.compose.ui.window.PopupPositionProvider
 import androidx.compose.ui.window.PopupProperties
-import com.composables.core.Dialog
-import com.composables.core.DialogPanel
-import com.composables.core.Scrim
-import com.composables.core.rememberDialogState
 
 @Composable
 internal fun PickerPopup(
     anchorAlignment: Alignment.Horizontal,
-    anchorBounds: Rect,
-    popupDrawArea: Rect? = null,
+    offset: DpOffset,
     onDismissRequest: () -> Unit,
     popupItems: List<PickerPopupItem>,
 ) {
     PickerPopup(
         anchorAlignment = anchorAlignment,
-        anchorBounds = anchorBounds,
-        popupDrawArea = popupDrawArea,
+        dpOffset = offset,
         onDismissRequest = onDismissRequest,
         popupItems = popupItems,
         state = remember {
@@ -63,59 +57,43 @@ internal fun PickerPopup(
 @Composable
 private fun PickerPopup(
     anchorAlignment: Alignment.Horizontal,
-    anchorBounds: Rect,
-    popupDrawArea: Rect? = null,
+    dpOffset: DpOffset,
     onDismissRequest: () -> Unit,
     popupItems: List<PickerPopupItem>,
     state: MutableTransitionState<Boolean>,
 ) {
+    val density = LocalDensity.current
+    val positionProvider = remember(density) { PickerPopupPositionProvider(density, anchorAlignment, dpOffset) }
     val cornerRadius = 8.dp
-    // full screen background
-    Dialog(state = rememberDialogState(initiallyVisible = true)) {
-        Scrim(scrimColor = Color.Black.copy(alpha = 0.2f))
-        DialogPanel(
-            modifier = Modifier
-                .fillMaxSize(),
-        ) {
-            var popupSize by remember { mutableStateOf(IntSize.Zero) }
 
-            Popup(
-                alignment = Alignment.TopStart,
-                onDismissRequest = onDismissRequest,
-                offset = IntOffset(
-                    calculatePopupXOffset(anchorAlignment, anchorBounds, popupDrawArea, popupSize),
-                    (anchorBounds.top - popupSize.height - 10.dp.dpToPx()).toInt(),
-                ),
-                properties = PopupProperties(focusable = true),
+    Popup(
+        onDismissRequest = onDismissRequest,
+        popupPositionProvider = positionProvider,
+        properties = PopupProperties(focusable = true),
+    ) {
+        AnimatedVisibility(
+            visibleState = state,
+            enter = scaleIn(animationSpec = spring(stiffness = Spring.StiffnessMedium)),
+        ) {
+            Surface(
+                modifier = Modifier
+                    .fillMaxWidth(0.6f),
+                shape = RoundedCornerShape(cornerRadius),
+                tonalElevation = 3.dp,
+                shadowElevation = 2.dp,
             ) {
-                AnimatedVisibility(
-                    visibleState = state,
-                    enter = scaleIn(animationSpec = spring(stiffness = Spring.StiffnessMedium)),
-                ) {
-                    Surface(
-                        modifier = Modifier
-                            .fillMaxWidth(0.6f)
-                            .onGloballyPositioned {
-                                popupSize = it.size
-                            },
-                        shape = RoundedCornerShape(cornerRadius),
-                        tonalElevation = 3.dp,
-                        shadowElevation = 2.dp,
-                    ) {
-                        LazyColumn {
-                            itemsIndexed(popupItems) { index, item ->
-                                PopupButton(
-                                    text = stringResource(item.text),
-                                    iconRes = item.iconRes,
-                                    contentDescription = stringResource(item.contentDescription),
-                                    shape = popupButtonShape(index, popupItems.size, cornerRadius),
-                                    color = item.color,
-                                    onClick = item.onClick,
-                                )
-                                if (index < popupItems.size - 1) {
-                                    HorizontalDivider()
-                                }
-                            }
+                LazyColumn {
+                    itemsIndexed(popupItems) { index, item ->
+                        PopupButton(
+                            text = stringResource(item.text),
+                            iconRes = item.iconRes,
+                            contentDescription = stringResource(item.contentDescription),
+                            shape = popupButtonShape(index, popupItems.size, cornerRadius),
+                            color = item.color,
+                            onClick = item.onClick,
+                        )
+                        if (index < popupItems.size - 1) {
+                            HorizontalDivider()
                         }
                     }
                 }
@@ -137,9 +115,6 @@ private fun popupButtonShape(index: Int, totalItems: Int, cornerRadius: Dp): Rou
     }
 }
 
-@Composable
-private fun Dp.dpToPx() = with(LocalDensity.current) { this@dpToPx.toPx() }
-
 internal data class PickerPopupItem(
     @StringRes val text: Int,
     @DrawableRes val iconRes: Int,
@@ -148,19 +123,64 @@ internal data class PickerPopupItem(
     val onClick: () -> Unit,
 )
 
-private fun calculatePopupXOffset(
-    anchorAlignment: Alignment.Horizontal,
-    anchorBounds: Rect,
-    maxBounds: Rect? = null,
-    popupSize: IntSize,
-): Int {
-    val offset = when (anchorAlignment) {
-        Alignment.Start -> anchorBounds.left.toInt()
-        Alignment.End -> (anchorBounds.right - popupSize.width).toInt()
-        else -> (anchorBounds.left.toInt() + anchorBounds.width.toInt() / 2) - (popupSize.width / 2)
-    }
+// Code modified from Compose-Unstyled Menu.kt to prioritize positioning above the anchor
+// https://github.com/composablehorizons/compose-unstyled/blob/c62bab5babdabceb634ec66de05f5370c161b66d/core/src/commonMain/kotlin/Menu.kt
+@Immutable
+internal data class PickerPopupPositionProvider(
+    val density: Density,
+    val alignment: Alignment.Horizontal,
+    val offset: DpOffset = DpOffset.Zero,
+) : PopupPositionProvider {
+    override fun calculatePosition(
+        anchorBounds: IntRect,
+        windowSize: IntSize,
+        layoutDirection: LayoutDirection,
+        popupContentSize: IntSize,
+    ): IntOffset {
+        val intOffset: IntOffset = with(density) {
+            IntOffset(offset.x.toPx().toInt(), offset.y.toPx().toInt())
+        }
 
-    return maxBounds?.let { bounds ->
-        offset.coerceIn(bounds.left.toInt(), (bounds.right - popupSize.width).toInt())
-    } ?: offset
+        // Compute horizontal position.
+        val toRight = anchorBounds.left
+        val toLeft = anchorBounds.right - popupContentSize.width
+
+        val toDisplayRight = windowSize.width - popupContentSize.width
+        val toDisplayLeft = 0
+
+        val x = (
+            if (alignment == Alignment.Start) {
+                sequenceOf(
+                    toRight,
+                    toLeft,
+                    // If the anchor gets outside of the window on the left, we want to position
+                    // toDisplayLeft for proximity to the anchor. Otherwise, toDisplayRight.
+                    if (anchorBounds.left >= 0) toDisplayRight else toDisplayLeft,
+                )
+            } else if (alignment == Alignment.End) {
+                sequenceOf(
+                    toLeft,
+                    toRight,
+                    // If the anchor gets outside of the window on the right, we want to position
+                    // toDisplayRight for proximity to the anchor. Otherwise, toDisplayLeft.
+                    if (anchorBounds.right <= windowSize.width) toDisplayLeft else toDisplayRight,
+                )
+            } else { // middle
+                sequenceOf(anchorBounds.left + (anchorBounds.width - popupContentSize.width) / 2)
+            }
+        ).firstOrNull {
+            it >= 0 && it + popupContentSize.width <= windowSize.width
+        } ?: toLeft
+
+        // Compute vertical position.
+        val toBottom = maxOf(anchorBounds.bottom + intOffset.y, 0)
+        val toTop = anchorBounds.top - intOffset.y - popupContentSize.height
+        val toCenter = anchorBounds.top - intOffset.y - popupContentSize.height / 2
+        val toDisplayBottom = windowSize.height - intOffset.y - popupContentSize.height
+        val y = sequenceOf(toTop, toBottom, toCenter, toDisplayBottom).firstOrNull {
+            it >= 0 && it + popupContentSize.height <= windowSize.height
+        } ?: toTop
+
+        return IntOffset(x, y)
+    }
 }
