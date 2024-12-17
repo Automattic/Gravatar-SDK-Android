4c5530aca92015d79bc27ee14fe46a0fef368f51
AdamGrzybkowski <agrzybkowski@outlook.com>
2024-12-17 21:05:18 +0100

    Update screenshot tests

--- gravatar-quickeditor/src/test/java/com/gravatar/quickeditor/ui/oauth/OAuthPageTest.kt
+++ gravatar-quickeditor/src/test/java/com/gravatar/quickeditor/ui/oauth/OAuthPageTest.kt
@@ -2,6 +2,7 @@ package com.gravatar.quickeditor.ui.oauth
 
 import com.gravatar.quickeditor.ui.gravatarScreenshotTest
 import com.gravatar.types.Email
+import com.gravatar.ui.components.ComponentState
 import com.gravatar.uitestutils.RoborazziTest
 import org.junit.Test
 import org.robolectric.annotation.Config
@@ -10,7 +11,9 @@ class OAuthPageTest : RoborazziTest() {
     @Test
     fun oAuthPageLight() = gravatarScreenshotTest {
         OauthPage(
-            uiState = OAuthUiState(),
+            uiState = OAuthUiState(
+                profile = ComponentState.Empty,
+            ),
             email = Email("email"),
             onStartOAuthClicked = {},
         )
@@ -21,10 +24,24 @@ class OAuthPageTest : RoborazziTest() {
     fun oAuthPageDark() {
         gravatarScreenshotTest {
             OauthPage(
-                uiState = OAuthUiState(),
+                uiState = OAuthUiState(
+                    profile = ComponentState.Empty,
+                ),
                 email = Email("email"),
                 onStartOAuthClicked = {},
             )
         }
+    }
+
+    @Test
+    fun oAuthPageAuthorizing() = gravatarScreenshotTest {
+        OauthPage(
+            uiState = OAuthUiState(
+                profile = ComponentState.Empty,
+                status = OAuthStatus.Authorizing,
+            ),
+            email = Email("email"),
+            onStartOAuthClicked = {},
+        )
     }
 }
