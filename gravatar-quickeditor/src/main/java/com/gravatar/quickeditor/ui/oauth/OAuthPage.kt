05ead090bbe823049ffd37efe69ff7ea55cb002e
Adam Grzybkowski <agrzybkowski@outlook.com>
2025-01-22 15:09:54 +0100

    Update gravatar-quickeditor/src/main/java/com/gravatar/quickeditor/ui/oauth/OAuthPage.kt
    Co-authored-by: Maxime Lumeau <maxime.lumeau@automattic.com>

--- gravatar-quickeditor/src/main/java/com/gravatar/quickeditor/ui/oauth/OAuthPage.kt
+++ gravatar-quickeditor/src/main/java/com/gravatar/quickeditor/ui/oauth/OAuthPage.kt
@@ -78,7 +78,7 @@ internal fun OAuthPage(
             val listener = Consumer<Intent> { newIntent ->
                 Log.w(
                     "QuickEditor",
-                    "Setup GravatarOAuthActivity in your AndroidManifest.xml to handle the OAuth flow.",
+                    "GRAVATAR SDK WARNING: You're using a deprecated version of the Gravatar QuickEditor OAuth flow. Set up GravatarOAuthActivity in your AndroidManifest.xml to handle the OAuth flow and to remove this warning message. ",
                 )
                 val token = newIntent.data
                     ?.encodedFragment
