519c47e0828682cc3ca99e4237aba005c3d3d584
AdamGrzybkowski <agrzybkowski@outlook.com>
2025-03-24 09:42:49 +0100

    Create ProfileRepository to get profile with oauth token

---
+++ gravatar-quickeditor/src/main/java/com/gravatar/quickeditor/data/repository/ProfileRepository.kt
@@ -0,0 +1,26 @@
+package com.gravatar.quickeditor.data.repository
+
+import com.gravatar.quickeditor.data.models.QuickEditorError
+import com.gravatar.quickeditor.data.storage.TokenStorage
+import com.gravatar.restapi.models.Profile
+import com.gravatar.services.GravatarResult
+import com.gravatar.services.ProfileService
+import com.gravatar.types.Email
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.withContext
+
+internal class ProfileRepository(
+    private val tokenStorage: TokenStorage,
+    private val profileService: ProfileService,
+    private val dispatcher: CoroutineDispatcher,
+) {
+    suspend fun getProfile(email: Email): GravatarResult<Profile, QuickEditorError> = withContext(dispatcher) {
+        val token = tokenStorage.getToken(email.hash().toString())
+        token?.let {
+            when (val result = profileService.retrieveProfileCatching(token)) {
+                is GravatarResult.Success -> GravatarResult.Success(result.value)
+                is GravatarResult.Failure -> GravatarResult.Failure(QuickEditorError.Request(result.error))
+            }
+        } ?: GravatarResult.Failure(QuickEditorError.TokenNotFound)
+    }
+}
---
+++ gravatar-quickeditor/src/test/java/com/gravatar/quickeditor/data/repository/ProfileRepositoryTest.kt
@@ -0,0 +1,84 @@
+package com.gravatar.quickeditor.data.repository
+
+import com.gravatar.extensions.defaultProfile
+import com.gravatar.quickeditor.data.models.QuickEditorError
+import com.gravatar.quickeditor.data.storage.TokenStorage
+import com.gravatar.quickeditor.ui.CoroutineTestRule
+import com.gravatar.quickeditor.ui.avatarpicker.EmailAvatars
+import com.gravatar.restapi.models.Profile
+import com.gravatar.services.ErrorType
+import com.gravatar.services.GravatarResult
+import com.gravatar.services.ProfileService
+import com.gravatar.types.Email
+import io.mockk.coEvery
+import io.mockk.coVerify
+import io.mockk.mockk
+import kotlinx.coroutines.test.StandardTestDispatcher
+import kotlinx.coroutines.test.runTest
+import org.junit.Assert.assertEquals
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+
+class ProfileRepositoryTest {
+    private val testDispatcher = StandardTestDispatcher()
+
+    @get:Rule
+    var coroutinesTestRule = CoroutineTestRule(testDispatcher)
+
+    private lateinit var profileRepository: ProfileRepository
+    private val profileService: ProfileService = mockk()
+    private val tokenStorage: TokenStorage = mockk()
+
+    private val email = Email("email")
+
+    @Before
+    fun setUp() {
+        profileRepository = ProfileRepository(
+            profileService = profileService,
+            tokenStorage = tokenStorage,
+            dispatcher = testDispatcher,
+        )
+    }
+
+    @Test
+    fun `given email when token not found then TokenNotFound result`() = runTest {
+        coEvery { tokenStorage.getToken(any()) } returns null
+
+        val result = profileRepository.getProfile(email)
+
+        assertEquals(
+            GravatarResult.Failure<EmailAvatars, QuickEditorError>(QuickEditorError.TokenNotFound),
+            result,
+        )
+    }
+
+    @Test
+    fun `given oauthToken when retrieving profile and data is returned then result is successful`() = runTest {
+        val oauthToken = "oauth"
+        val profileResult = defaultProfile(hash = "hash")
+        coEvery { tokenStorage.getToken(any()) } returns oauthToken
+        coEvery {
+            profileService.retrieveProfileCatching(oauthToken)
+        } returns GravatarResult.Success(profileResult)
+
+        val profile = profileRepository.getProfile(email)
+
+        coVerify(exactly = 1) { profileService.retrieveProfileCatching(oauthToken) }
+        assertEquals(GravatarResult.Success<Profile, QuickEditorError>(profileResult), profile)
+    }
+
+    @Test
+    fun `given oauthToken when retrieving profile and response is NOT successful then Failure returned`() = runTest {
+        val oauthToken = "oauth"
+        coEvery { tokenStorage.getToken(any()) } returns oauthToken
+        coEvery { profileService.retrieveProfileCatching(oauthToken) } returns GravatarResult.Failure(ErrorType.Server)
+
+        val result = profileRepository.getProfile(email)
+
+        assertEquals(
+            GravatarResult.Failure<Profile, QuickEditorError>(QuickEditorError.Request(ErrorType.Server)),
+            result,
+        )
+    }
+}
