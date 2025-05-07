3285c820b8ffb94555056b1d49cc6680716f6128
AdamGrzybkowski <agrzybkowski@outlook.com>
2025-05-07 14:38:27 +0200

    Increase the EditButton's size to the min touchable area

--- gravatar-quickeditor/src/main/java/com/gravatar/quickeditor/ui/components/ProfileCard.kt
+++ gravatar-quickeditor/src/main/java/com/gravatar/quickeditor/ui/components/ProfileCard.kt
@@ -3,22 +3,27 @@ package com.gravatar.quickeditor.ui.components
 import android.content.res.Configuration
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.ripple.rememberRipple
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.minimumInteractiveComponentSize
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -87,10 +92,12 @@ internal fun ProfileCard(
                                     EditButton(
                                         onClick = onEditAvatarClicked,
                                         backgroundColor = backgroundColor,
+                                        contentAlignment = Alignment.BottomEnd,
                                         contentDescription = stringResource(
                                             R.string.gravatar_qe_edit_avatar_content_description,
                                         ),
                                         modifier = Modifier
+                                            .offset(12.dp, 12.dp)
                                             .align(Alignment.BottomEnd),
                                     )
                                 }
@@ -110,10 +117,13 @@ internal fun ProfileCard(
                     EditButton(
                         onClick = onEditAboutClicked,
                         backgroundColor = backgroundColor,
+                        contentAlignment = Alignment.TopEnd,
                         contentDescription = stringResource(
                             R.string.gravatar_qe_edit_about_content_description,
                         ),
-                        modifier = Modifier.padding(top = 5.dp),
+                        modifier = Modifier
+                            .offset(x = 12.dp, y = (-12).dp)
+                            .padding(top = 5.dp),
                     )
                 }
             }
@@ -150,18 +160,32 @@ private fun EditButton(
     backgroundColor: Color,
     contentDescription: String?,
     modifier: Modifier = Modifier,
+    contentAlignment: Alignment = Alignment.Center,
 ) {
-    Icon(
-        painter = painterResource(R.drawable.ic_edit),
-        contentDescription = contentDescription,
-        tint = MaterialTheme.colorScheme.onSurface,
+    Box(
         modifier = modifier
-            .size(24.dp)
-            .clip(CircleShape)
-            .background(backgroundColor)
-            .padding(2.dp)
-            .clickable { onClick() },
-    )
+            .minimumInteractiveComponentSize()
+            .clickable(
+                onClick = onClick,
+                interactionSource = remember { MutableInteractionSource() },
+                indication = rememberRipple(
+                    bounded = false,
+                    radius = 48.dp / 2,
+                ),
+            ),
+        contentAlignment = contentAlignment,
+    ) {
+        Icon(
+            painter = painterResource(R.drawable.ic_edit),
+            contentDescription = contentDescription,
+            tint = MaterialTheme.colorScheme.onSurface,
+            modifier = Modifier
+                .size(24.dp)
+                .clip(CircleShape)
+                .background(backgroundColor)
+                .padding(2.dp),
+        )
+    }
 }
 
 @Preview(showBackground = true, heightDp = 200)
