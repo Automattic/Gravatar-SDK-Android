6f920da154fee74b039b144738bff31deb3d56dc
AdamGrzybkowski <agrzybkowski@outlook.com>
2025-05-26 11:15:21 +0200

    Add first&last name fields to the UI

--- gravatar-quickeditor/src/main/java/com/gravatar/quickeditor/ui/abouteditor/components/AboutFieldsSection.kt
+++ gravatar-quickeditor/src/main/java/com/gravatar/quickeditor/ui/abouteditor/components/AboutFieldsSection.kt
@@ -2,11 +2,16 @@ package com.gravatar.quickeditor.ui.abouteditor.components
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.composeunstyled.Text
 import com.gravatar.quickeditor.ui.abouteditor.AboutEditorField
+import com.gravatar.quickeditor.ui.editor.AboutInputField
 
 @Composable
 internal fun AboutFieldsSection(
@@ -15,15 +20,26 @@ internal fun AboutFieldsSection(
     formEnabled: Boolean,
     onValueChange: (AboutEditorField) -> Unit,
     modifier: Modifier = Modifier,
+    sectionDescription: String? = null,
 ) {
     Column(
         modifier = modifier,
         verticalArrangement = Arrangement.spacedBy(16.dp),
     ) {
         label?.let {
-            AboutEditSectionLabel(
-                title = it,
-            )
+            Column {
+                AboutEditSectionLabel(
+                    title = it,
+                )
+                sectionDescription?.let {
+                    Text(
+                        text = sectionDescription,
+                        style = MaterialTheme.typography.bodySmall,
+                        color = MaterialTheme.colorScheme.onSurfaceVariant,
+                        modifier = Modifier.padding(top = 4.dp),
+                    )
+                }
+            }
         }
         fields.forEach { editorField ->
             AboutEditField(
@@ -38,4 +54,26 @@ internal fun AboutFieldsSection(
             )
         }
     }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun AboutFieldsSectionPreview() {
+    AboutFieldsSection(
+        label = "Extras",
+        sectionDescription = "This information will not appear on your Gravatar " +
+            "Web Profile, but other apps and services can use it.",
+        fields = setOf(
+            AboutEditorField(
+                type = AboutInputField.FirstName,
+                value = "John",
+            ),
+            AboutEditorField(
+                type = AboutInputField.LastName,
+                value = "Doe",
+            ),
+        ),
+        formEnabled = true,
+        onValueChange = {},
+    )
 }
