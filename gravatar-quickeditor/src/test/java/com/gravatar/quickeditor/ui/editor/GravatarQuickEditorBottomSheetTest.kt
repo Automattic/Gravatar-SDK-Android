85b54e3008e0803a354e696d4667b409e11ad87a
Héctor Abraham <hector.abraham.morillo.prieto@automattic.com>
2025-06-12 09:07:48 +0200

    Add the avatar picker as content in the GravatarModalBottomSheetTests

--- gravatar-quickeditor/src/test/java/com/gravatar/quickeditor/ui/editor/GravatarQuickEditorBottomSheetTest.kt
+++ gravatar-quickeditor/src/test/java/com/gravatar/quickeditor/ui/editor/GravatarQuickEditorBottomSheetTest.kt
@@ -1,14 +1,19 @@
 package com.gravatar.quickeditor.ui.editor
 
-import androidx.compose.ui.graphics.Color
+import androidx.compose.runtime.Composable
 import com.composables.core.rememberModalBottomSheetState
-import com.composeunstyled.Text
+import com.gravatar.quickeditor.ui.avatarpicker.AvatarPicker
+import com.gravatar.quickeditor.ui.avatarpicker.AvatarPickerUiState
+import com.gravatar.quickeditor.ui.avatarpicker.EmailAvatars
 import com.gravatar.quickeditor.ui.editor.bottomsheet.GravatarModalBottomSheet
 import com.gravatar.quickeditor.ui.editor.bottomsheet.modalDetents
 import com.gravatar.quickeditor.ui.gravatarScreenshotTest
+import com.gravatar.restapi.models.Avatar
+import com.gravatar.types.Email
 import com.gravatar.uitestutils.RoborazziTest
 import org.junit.Test
 import org.robolectric.annotation.Config
+import java.net.URI
 
 class GravatarQuickEditorBottomSheetTest : RoborazziTest() {
     @Test
@@ -17,7 +22,7 @@ class GravatarQuickEditorBottomSheetTest : RoborazziTest() {
             colorScheme = GravatarUiMode.LIGHT,
             modalBottomSheetState = rememberModalBottomSheetState(modalDetents().initialDetent),
         ) {
-            Text("Gravatar Quick Editor Bottom Sheet")
+            TestContent()
         }
     }
 
@@ -28,7 +33,37 @@ class GravatarQuickEditorBottomSheetTest : RoborazziTest() {
             colorScheme = GravatarUiMode.DARK,
             modalBottomSheetState = rememberModalBottomSheetState(modalDetents().initialDetent),
         ) {
-            Text(color = Color.White, text = "Gravatar Quick Editor Bottom Sheet")
+            TestContent()
         }
+    }
+
+    @Composable
+    private fun TestContent() {
+        AvatarPicker(
+            uiState = AvatarPickerUiState(
+                email = Email("william.henry.harrison@example.com"),
+                avatarPickerContentLayout = AvatarPickerContentLayout.Horizontal,
+                emailAvatars = EmailAvatars(
+                    avatars = listOf(
+                        Avatar {
+                            imageUrl = URI.create("https://gravatar.com/avatar/test")
+                            imageId = "1"
+                            rating = Avatar.Rating.G
+                            altText = "alt"
+                            updatedDate = ""
+                        },
+                        Avatar {
+                            imageUrl = URI.create("https://gravatar.com/avatar/test2")
+                            imageId = "2"
+                            rating = Avatar.Rating.G
+                            altText = "alt"
+                            updatedDate = ""
+                        },
+                    ),
+                    selectedAvatarId = "1",
+                ),
+            ),
+            onEvent = { },
+        )
     }
 }
